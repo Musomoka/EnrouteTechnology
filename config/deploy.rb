2ae75155cee09d8ba3adc47ffc24d3115f8d0e0c
Musomoka <musomoka@gmail.com>
2018-12-26 01:57:22 +0200

    initial nginx cap pum setup again

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
-# config valid for current version and patch releases of Capistrano
-lock "~> 3.11.0"
 
-server '68.183.78.91', port: 22, roles: [:web, :app, :db], primary: true
+# Change these
+server '68.183.78.91', port: 80, roles: [:web, :app, :db], primary: true
 
-set :application, "EnrouteTechnology"
-set :repo_url, "https://github.com/Musomoka/EnrouteTechnology.git"
-set :user, 'rails'
+set :repo_url,        'https://github.com/Musomoka/EnrouteTechnology.git'
+set :application,     'enroutetechnology'
+set :user,            'deploy'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
+# Don't change these unless you know what you're doing
 set :pty,             true
 set :use_sudo,        false
 set :stage,           :production
@@ -24,7 +24,6 @@ set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
-
 ## Defaults:
 # set :scm,           :git
 # set :branch,        :master
@@ -37,50 +36,50 @@ set :puma_init_active_record, true  # Change to false when not using ActiveRecor
 # set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :puma do
-    desc 'Create Directories for Puma Pids and Socket'
-    task :make_dirs do
-      on roles(:app) do
-        execute "mkdir #{shared_path}/tmp/sockets -p"
-        execute "mkdir #{shared_path}/tmp/pids -p"
-      end
+  desc 'Create Directories for Puma Pids and Socket'
+  task :make_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
     end
-  
-    before :start, :make_dirs
   end
-  
-  namespace :deploy do
-    desc "Make sure local git is in sync with remote."
-    task :check_revision do
-      on roles(:app) do
-        unless `git rev-parse HEAD` == `git rev-parse origin/master`
-          puts "WARNING: HEAD is not the same as origin/master"
-          puts "Run `git push` to sync changes."
-          exit
-        end
+
+  before :start, :make_dirs
+end
+
+namespace :deploy do
+  desc "Make sure local git is in sync with remote."
+  task :check_revision do
+    on roles(:app) do
+      unless `git rev-parse HEAD` == `git rev-parse origin/master`
+        puts "WARNING: HEAD is not the same as origin/master"
+        puts "Run `git push` to sync changes."
+        exit
       end
     end
-  
-    desc 'Initial Deploy'
-    task :initial do
-      on roles(:app) do
-        before 'deploy:restart', 'systemctl restart rails.service'
-        invoke 'deploy'
-      end
-    end
-  
-    desc 'Restart application'
-    task :restart do
-      on roles(:app), in: :sequence, wait: 5 do
-        invoke 'systemctl restart rails.service'
-      end
-    end
-  
-    before :starting,     :check_revision
-    after  :finishing,    :compile_assets
-    after  :finishing,    :cleanup
-    after  :finishing,    :restart
   end
-  
-  # ps aux | grep puma    # Get puma pid
-  # kill -s SIGUSR2 pid   # Restart puma
-  # kill -s SIGTERM pid   # Stop puma
+
+  desc 'Initial Deploy'
+  task :initial do
+    on roles(:app) do
+      before 'deploy:restart', 'puma:start'
+      invoke 'deploy'
+    end
+  end
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      invoke 'puma:restart'
+    end
+  end
+
+  before :starting,     :check_revision
+  after  :finishing,    :compile_assets
+  after  :finishing,    :cleanup
+  after  :finishing,    :restart
+end
+
+# ps aux | grep puma    # Get puma pid
+# kill -s SIGUSR2 pid   # Restart puma
+# kill -s SIGTERM pid   # Stop puma
