cf1c105b2d4dcab713991cd2904e7b4bd25043b2
Musomoka <musomoka@gmail.com>
2018-12-26 15:30:34 +0200

    master key rails

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,8 +23,6 @@ set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
-set :master_key_local_path, "/home/#{fetch(:user)}/apps/repo_path/config/master.key
-
 
 
 
