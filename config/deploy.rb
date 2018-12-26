c4894f0fd52f65dc8744aa8cc20a39fd64567858
Musomoka <musomoka@gmail.com>
2018-12-26 05:03:02 +0200

    edit secret key

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,6 +24,13 @@ set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
+
+set :default_env, {
+  "RAILS_ENV" => "production",
+  "RAILS_MASTER_KEY" => ENV["RAILS_MASTER_KEY"]
+}
+
+append :linked_files, "config/master.key"
 ## Defaults:
 # set :scm,           :git
 # set :branch,        :master
