832c21cb6c823b8a72cb67e3bc93e274baed4d8d
Musomoka <musomoka@gmail.com>
2018-12-26 16:27:59 +0200

    key repair

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,7 @@ Rails.application.configure do
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
    config.require_master_key = true
 
-   ENV[“RAILS_MASTER_KEY”] = 6a59442a5b44d85b17ba702e2371d7ea
+   ENV[“RAILS_MASTER_KEY”] = secret_key_base
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
