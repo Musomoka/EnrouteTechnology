309660dbb23f8b1d55c59d92d13a91a6462c009c
Musomoka <musomoka@gmail.com>
2018-12-27 14:12:07 +0200

    stil fixing masterkey

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,14 +18,14 @@ Rails.application.configure do
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
    config.require_master_key = true
 
-  
+   ENV(RAILS_MASTER_KEY) = config.master.key
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  # config.assets.css_compressor = :sassexi
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
